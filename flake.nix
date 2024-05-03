cc6ef303a5350ea7618815dfcf8a2c1bce76f8eb
Ricardo Antunes <me@riscadoa.com>
2024-05-03 10:30:45 +0100

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import inputs.nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          hardeningDisable = [ "all" ];
+
+          packages = with pkgs; [
+            (python39.withPackages (ps: [
+              ps.pygame
+            ]))
+          ];
+        };
+      });
+}
