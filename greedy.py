371ad5a30a7f9e8bcf5674b9e9e9f0354a7e1695
Goncalo-Palma-Nunes <goncalopalmanunes@tecnico.ulisboa.pt>
2024-05-12 19:28:17 +0100

    refactor: move closestApple() to agents.py

--- greedy.py
+++ greedy.py
@@ -1,29 +1,8 @@
-from agents import Agent, UP, DOWN, LEFT, RIGHT, SIGHT_RADIUS
+from agents import Agent, UP, DOWN, LEFT, RIGHT
 
 class GreedyAgent(Agent):
     def __init__(self, env, endowment, utility_function=lambda x: 1):
         Agent.__init__(self, env, endowment, utility_function)
-
-
-    def closestApple(self):
-        i, j = self.getPosition()
-
-        closest = None
-        closest_distance = float('inf')
-        size = self.getEnv().getBoardSize()
-        # Iterate over the board
-        for x in range(size):
-            for y in range(size):
-                # Check if the cell has a resource
-                if self.getEnv().getCell(x, y).hasResource():
-                    # Calculate the distance between the agent and the cell
-                    distance = self.getEnv().manhattanDistance(i, j, x, y)
-                    # If the distance is less than the closest distance, update the closest cell
-                    if distance < closest_distance:
-                        closest = (x, y)
-                        closest_distance = distance
-
-        return closest
     
     def act(self):
         self.perceive(self.getEnv())
