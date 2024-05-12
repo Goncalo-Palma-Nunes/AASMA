35b39ff50a8568184be2bb3362ef9b06954572a1
Goncalo-Palma-Nunes <goncalopalmanunes@tecnico.ulisboa.pt>
2024-05-12 20:14:12 +0100

    feat: compute plan

--- greedy.py
+++ greedy.py
@@ -1,11 +1,75 @@
 from agents import Agent, UP, DOWN, LEFT, RIGHT
 
+#######################
+###     Class       ###
+#######################
+
 class GreedyAgent(Agent):
+
+    ###########################
+    ###       Attributes    ###
+    ###########################
+
+    target_position = None
+    plan = []
+
+    ###########################
+    ###     Constructor     ###
+    ###########################
+
     def __init__(self, env, endowment, utility_function=lambda x: 1):
         Agent.__init__(self, env, endowment, utility_function)
+
+    ###########################
+    ### Getters and setters ###
+    ###########################
+
+    def getTargetPosition(self):
+        return self.target_position
     
+    def setTargetPosition(self, target_position):
+        if not isinstance(target_position, (tuple, list)) \
+            or not len(target_position) == 2 \
+            or not all(isinstance(i, int) for i in target_position):
+            raise ValueError("Target position must be a list or tuple of length 2 containing integers.")
+        
+        if any (i > self.getEnv().getBoardSize() for i in target_position) \
+            or any (i < 0 for i in target_position):
+            raise ValueError("Target position must be within the bounds of the board.")
+        
+        self.target_position = target_position
+
+    def getPlan(self):
+        return self.plan
+    
+    def setPlan(self, plan):
+        if not isinstance(plan, list):
+            raise ValueError("Plan must be a list.")
+        self.plan = plan
+
+    def topPlan(self):
+        return self.getPlan()[0]
+    
+    def getTargetFromPlan(self):
+        return self.getPlan()[-1]
+
+    def popPlan(self):
+        return self.getPlan().pop(0)
+    
+    ###########################
+    ###     Methods         ###
+    ###########################
+
+    def noTargetPosition(self):
+        return self.getTargetPosition() is None
+
     def act(self):
         self.perceive(self.getEnv())
+
+        if self.noTargetPosition() :
+            self.setPlan(self.pathToClosestApple())
+            self.setTargetPosition(self.getTargetFromPlan())
+
 
         raise NotImplementedError
 
