c85392800a5d630a8aa1d1885113111ccc46a4f0
Goncalo-Palma-Nunes <goncalopalmanunes@tecnico.ulisboa.pt>
2024-05-16 21:18:06 +0100

    feat: implement computeSustainableConsumption

--- cooperative.py
+++ cooperative.py
@@ -1,14 +1,22 @@
 from deliberative_agent import DeliberativeAgent
 
 class CooperativeAgent(DeliberativeAgent):
+    sustainable_consumption = 0
+
     def __init__(self, env, endowment, utility_function=lambda x: 1):
         DeliberativeAgent.__init__(self, env, endowment, utility_function)
+        self.sustainable_consumption = self.computeSustainableConsumption()
 
     def computeSustainableConsumption(self):
         boardsize = self.getEnv().getBoardSize()
         num_players = len(self.getOtherPlayers()) + 1
         num_apples = self.getBoard().getNumApples()
         growth_frequency = self.getBoard().getResourceGrowthFrequency()
+
+        sustainable_consumption = num_apples + (growth_frequency) * num_apples / (boardsize - num_apples)
+        sustainable_consumption = sustainable_consumption / (2 * num_players)
+
+        return sustainable_consumption
         
     def act(self):
         raise NotImplementedError("Cooperative agents must implement the act method.")
