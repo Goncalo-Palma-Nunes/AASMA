c73fbfcf284b83def3a0d2c88fca688165ca46e6
Goncalo-Palma-Nunes <goncalopalmanunes@tecnico.ulisboa.pt>
2024-05-17 15:20:54 +0100

    feat: greedy behaves cooperatively when near other agents

--- src/greedy.py
+++ src/greedy.py
@@ -1,52 +1,32 @@
-from deliberative_agent import DeliberativeAgent
+from cooperative import CooperativeAgent
 
 #######################
 ###     Class       ###
 #######################
 
-class GreedyAgent(DeliberativeAgent):
+class GreedyAgent(CooperativeAgent):
 
     ###########################
     ###     Constructor     ###
     ###########################
 
     def __init__(self, env, endowment, utility_function=lambda x: 1):
-        DeliberativeAgent.__init__(self, env, endowment, utility_function)
+        CooperativeAgent.__init__(self, env, endowment, utility_function)
 
     ###########################
     ###     Methods         ###
     ###########################
 
     def act(self):
-        consume_apple = False
-        self.targetStillValid()
-
-        if self.noTargetPosition() : # No current target/plan
-            self.setPlan(self.pathToClosestApple())
-            if self.getPlan() == []:
-                # If no apples are reachable, move in a random direction
-                self.moveInRandomDirection()
-                self.communicate(apple_consumed=consume_apple)
-                return
-
-            self.setTargetPosition(self.getTargetFromPlan())
-        else:
-            # Look for a closer apple in the vicinity
-            possible_new_target = self.closestAppleInRadius()
-            if possible_new_target is not None:
-                # If one exists, it is the closest apple and should be the target
-                i, j = self.getPosition()
-                x, y = possible_new_target
-                self.setPlan(self.getBoard().shortestPath(i, j, x, y))
-                self.setTargetPosition(self.getTargetFromPlan())
-
-        self.move(self.positionToDirection(self.popPlan())) # Move in the direction of the next move in the plan
-        if self.getPlan() == []:
-        # If the plan is empty, we have reached the target position
-            self.setTargetPosition(None)
-            consume_apple = bool(self.eat())
-
-        self.communicate(apple_consumed=consume_apple)
+        if self.anyAgentsInRadius():
+            # Mimic behaviour of cooperative agent
+            return super(GreedyAgent, self).act()
+        
+        # otherwise move towards the closest apple
+        if self.onResource():
+            return self.eat()
+        
+        return self.moveTowardsApple()
 
     def accuse(self):
         raise NotImplementedError
