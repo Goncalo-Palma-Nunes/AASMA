b3f06fe4c9165a7f46248cf473cfd44f58d037b7
jiyiru <inesyeji@gmail.com>
2024-05-23 07:38:51 +0100

    chore: cleanup the code

--- src/graph_stats.py
+++ src/graph_stats.py
@@ -19,25 +19,18 @@ class Graph_Stats:
             y = [stats_list[j][agent_types[i]] for j in range(self.stats.getStatsRound() + 1)]
             plt.plot(x, y, label = f"Agent {agent_types[i]}")
  
-        # naming the x axis
         plt.xlabel('Rounds')
-        # naming the y axis
         plt.ylabel(f"{stats_type}")
-        # giving a title to my graph
         plt.title(f"Graph of {stats_type} in {self.stats.getStatsRound() + 1} Rounds")
- 
-        # show a legend on the plot
         plt.legend()
         
          # Set x-axis ticks to integer values
         plt.xticks(range(1, self.stats.getStatsRound() + 2))
  
-        # function to save the plot
         plt.savefig(f'graphs/{stats_type}_line_plot.png')
         plt.close()
         
     def draw_bar_plot(self, stats_type):
-        # x-coordinates of left sides of bars 
         stats_list = self.getStats().getStatsType(stats_type)
         agent_types = list(stats_list[0].keys())
         left = []
@@ -51,24 +44,18 @@ class Graph_Stats:
             height.extend([stats_list[j][agent_types[i]] for j in range(self.stats.getStatsRound() + 1)])
             tick_label.extend([f"r{round_number}" for round_number in range(1, self.stats.getStatsRound() + 2)])
             
-        print(left, height, tick_label)
-            # labels for bars
         # Generate a list of colors using a colormap
         colors = plt.cm.viridis(np.linspace(0, 1, len(agent_types)))
-        
         bar_colors = np.repeat(colors, self.stats.getStatsRound() + 1, axis=0)
-        # plotting a bar chart
+
         plt.bar(left, height, tick_label=tick_label, width=bar_width, color=bar_colors)
 
         legend_handles = [mpatches.Patch(color=colors[i], label=agent_types[i]) for i in range(len(agent_types))]
         plt.legend(handles=legend_handles, title="Agent Types")
-        # naming the y-axis
+        
         plt.ylabel(f"{stats_type}")
-        # naming the x-axis
         plt.xlabel(f"Rounds")
-        # plot title
         plt.title(f"Graph of {stats_type} in {self.stats.getStatsRound() + 1} Rounds")
         
-        # function to show the plot
         plt.savefig(f'graphs/{stats_type}_bar_plot.png')
         plt.close()
